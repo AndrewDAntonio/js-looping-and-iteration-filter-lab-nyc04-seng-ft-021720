3eeaad0118c2c442de2916de0853bb7bfd929960
AndrewDAntonio <github email address>
2020-03-30 10:05:08 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(arr,str) {
+    return arr.filter(function (ele) {return ele.toLowerCase() === str.toLowerCase();});
+} 
+
+function fuzzyMatch(arr, str) {
+    return arr.filter(function (ele) {return ele[0] === str[0] })
+}
+
+function matchName(arr, str) {
+    return arr.filter(function (ele) {return ele.name === str});
+}
